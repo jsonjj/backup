af0d10928f908f61da2ba662f940e94509213fe7
jonathan thomas <jonat@MacBook-Pro.attlocal.net>
2025-05-20 16:08:50 -0700

    Created getTimestamp function to allow for optional timestamp usage. Added log and extract capabilities. Updated instructions

--- README.md
+++ README.md
@@ -4,8 +4,11 @@ This is a way for me to play around with different commands as a way to mimic th
 
 This is not meant to be any better than git, but rather just a way to test my abilities.
 
+Put your repositories on a shared drive
+
 Steps to Install the Repository:
 
-1. Make sure you have git installed on your device
+1. Make sure you have git installed on your device: brew install git
 2. Open the Terminal and type: git clone https://github.com/jsonjj/backup.git
 3. Run backup.sh init <working_dir>
+4. For help: backup.sh --help
--- backup.sh
+++ backup.sh
@@ -10,6 +10,15 @@ if [ $1 = "init" ]; then
   exit 0
 fi
 
+getTimestamp() {
+    timestamp="$2"
+    if [ "$timestamp" = "" ]; then
+        desired_branch=`ls -1t ./.backups/${1}_backup_*.tar | head -n1`
+        timestamp=`echo "$desired_branch" | grep -o '[0-9]\+'`
+    fi
+    echo $timestamp
+}
+
 if [[ ! -d "./.backups" ]]; then
     echo "AWWWWW thats so sad :(. This is not a repository!"
     exit 1
@@ -22,37 +31,31 @@ if [ $1 = "commit" ]; then
 elif [ $1 = "cb" ]; then
     branch_timestamp="${2}"
     IFS='-' read -r branch timestamp <<< "$branch_timestamp"
+    timestamp=$(getTimestamp $branch $timestamp)
     ${0} sb ${branch} ${timestamp} # call sb
     ${0} commit ${3} "Branch from ${branch_timestamp}"
 elif [ $1 = "rb" ]; then
     rm -r ./.backups/${2}_backup_*
 elif [ $1 = "sb" ]; then
     rm -rf -- *
-    timestamp="${3}"
-    if [ "${timestamp}" = "" ]; then
-        desired_branch=`ls -1t ./.backups/${2}_backup_*.tar | head -n1`
-        timestamp=`echo "$desired_branch" | grep -o '[0-9]\+'`
-    fi
+    timestamp=$(getTimestamp $2 $3)
     tar xf ./.backups/${2}_backup_${timestamp}.tar
-elif [ $1 = "status" ]; then
+elif [ $1 = "log" ]; then
     more .backups/status.txt
+elif [ $1 = "extract" ]; then
+    branch_timestamp="${2}"
+    IFS='-' read -r branch timestamp <<< "$branch_timestamp"
+    timestamp=$(getTimestamp $branch $timestamp)
+    tar xf ./.backups/${branch}_backup_${timestamp}.tar ${3}
 else
     echo "Usage: $0 [options]"
     echo "Options:"
-    echo "  commit <branch_name> <message>          Commit the working dir"
-    echo "  cb <branch>-<timestamp> <new_branch>    Creates a new branch from branch-timestamp"
-    echo "  rb <branch_name>                        Remove the branch and all of its commits"
-    echo "  sb <branch_name> [timestamp]            Switching to branch-timestamp"
-    echo "  status                                  History of your commits"
+    echo "  commit <branch_name> <message>                      Commit the working dir"
+    echo "  cb <branch_name>-[timestamp] <new_branch_name>      Creates a new branch from branch-timestamp"
+    echo "  rb <branch_name>                                    Remove the branch and all of its commits"
+    echo "  sb <branch_name> [timestamp]                        Switching to branch-timestamp"
+    echo "  log                                                 History of your commits"
+    echo "  extract <branch_name>-[timestamp] <some/path>       Extract a file/dir into working dir"
     exit 1
 fi
 exit 0
-    
-# git status /
-# git commit /
-# switch branch /
-# create branch /
-# remove branch /
-# git push
-# git init /
-# git checkout <ref> -- <path/to/file>
